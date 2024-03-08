b3615e3d8867d3b70d85818c1c50644b4a6cba07
abdelkhalekalashker <abdoahm80@gmail.com>
2024-03-08 19:36:10 +0200

    add csv gem and create csv_importer class in lib dir

---
+++ lib/csv_importer.rb
@@ -0,0 +1,25 @@
+require 'csv'
+
+class CsvImporter
+  def self.movie_import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      country = Country.find_or_create_by(name: row[:country])
+
+      city = City.find_or_create_by(name: row[:city], country_id: country&.id)
+
+      director = Director.find_or_create_by(name: row[:director])
+
+      actor = Actor.find_or_create_by(name: row[:actor])
+
+      movie = Movie.find_or_create_by(movie: row[:Movie], year: row[:Year], description: row[:Description])
+
+      city_movie = CityMovie.find_or_create_by(movie_id: movie.id, city: city.id)
+
+      actor_movie = ActorMovie.find_or_create_by(actor_id: actor.id, movie_id: movie.id)
+
+      director_movie = DirectorMovie.find_or_create_by(director_id: director.id, movie_id: movie.id)
+
+    end
+  end
+
+end
