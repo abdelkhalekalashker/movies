05b4bf11d26e49ce690e3fd24f6e37139fa22cae
abdelkhalekalashker <abdoahm80@gmail.com>
2024-03-08 17:43:06 +0200

    generate city scaffold to generate table, model, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :cities
+  resources :countries
   resources :reviews
   devise_for :users
   resources :movies
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_08_153505) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_08_153739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.bigint "country_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["country_id"], name: "index_cities_on_country_id"
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
@@ -55,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_08_153505) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cities", "countries"
   add_foreign_key "reviews", "movies"
   add_foreign_key "reviews", "users"
 end
